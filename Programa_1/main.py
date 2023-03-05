5f3cc732a0e3e1b2ae4730e4bc6aec40e429e8ca
GXelarium <umalex26@gmail.com>
2023-03-04 19:46:04 -0600

    Menú casi terminado

--- Programa_1/main.py
+++ Programa_1/main.py
@@ -1,10 +1,30 @@
 import Newton
 import numpy as np
+import os
+
+def limpiar():
+        if os.name == "posix":
+                os.system ("clear")
+        elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+                os.system ("cls")
+
+def metodo(opcion):
+        X = list(map(float, input("Introduce el punto inicial separado por comas: ").split(',')))
+        max = int(input("Introduce el máximo de iteraciones: "))
+        tol = float(input("Introduce la tolerancia: "))
+        metodo = Newton.metodo(X,max,tol,opcion)
+        print(metodo)
+
 
 def menu():
     print(""" PROGRAMA 1: MÉTODO DE NEWTON RAPHSON
+
+Elaborado por:
+-López Medina Andrés Alejandro
+
+
         
-        Elíge un sistema de ecuaciones:
+        Elige un sistema de ecuaciones:
 
         1)   
                 f1(x,y) = x² + xy - 10 = 0
@@ -26,13 +46,14 @@ def menu():
 
        """)
 
-    opcion = int(input("Sistema número: "))
+    opcion = int(input("Sistema de ecuaciones número: "))
     if opcion > 0 and opcion < 5:
-        X = list(map(float, input("Introduce el punto inicial separado por comas: ").split(',')))
-        max = int(input("Introduce el máximo de iteraciones: "))
-        tol = float(input("Introduce la tolerancia: "))
-        metodo = Newton.metodo(X,max,tol,opcion)
-        print(metodo)
+        loop = True
+        while loop:
+                metodo(opcion)
+                estado = input("¿Quieres introducir otros valores? S/N: ").lower()
+                if estado != 's':
+                     loop = False
     else:
         print("Escoge un sistema válido")
 
@@ -44,8 +65,15 @@ def menu():
 
 
 if __name__ == '__main__':
+    loop = True
+    while loop:
+        limpiar()
+        menu()
+        estado = input("¿Quieres resolver otro sistema? S/N: ").lower()
+        if estado != 's':
+             loop = False
+        
 
-    menu()
     # X = np.array([1.5,1.5])
     # opcion = 1
     # prueba = Newton.metodo(X,op=opcion)
